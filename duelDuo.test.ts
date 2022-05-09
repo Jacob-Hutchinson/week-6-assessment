e1818fe08cfe219bb528e1ee80b40c5684343450
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-05-09 14:49:25 -0600

    added trello link

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -19,7 +19,35 @@ test('Title shows up when page loads', async () => {
     expect(displayed).toBe(true)
 
 })
+//scotts exapmles for the assessment 
 
-test('see if the draw button will be clicked', async () => {
-    const drawButton = await driver.findElement(By.id('Draw'))
+test('check that the draw button displays the dive with id = choices', async () => {
+    await driver.findElement(By.id('draw')).click()
+    
+    const choices = await driver.findElement(By.id('choices'))
+    
+    const displayed = await choices.isDisplayed()
+    
+    expect(displayed).toBe(true)
 })
+test('check the clicking an "add to duo" button displays to the player-duo', async () => {
+    await driver.findElement(By.id('draw')).click()
+
+    await driver.findElement(By.xpath('(//button[text()="add to duo])[1]')).click()
+
+    const playerDuo = await driver.findElement(By.id('player-duo'))
+
+    const displayed = await playerDuo.isDisplayed()
+
+    expect(displayed).toBe(true)
+
+
+
+})
+
+
+
+
+// test('see if the draw button will be clicked', async () => {
+//     const drawButton = await driver.findElement(By.id('Draw'))
+// })
--- functions.test.ts
+++ functions.test.ts
@@ -1,10 +1,26 @@
 const {shuffleArray} = require('./utils')
 
+let testArr = [1,2,3,4]
+
 describe('shuffleArray should', () => {
-    test('shuffleArray should do anything', () => {
-        expect(shuffleArray()).toHaveReturned()
-    })
+    // test('shuffleArray should do anything', () => {
+    //     expect(shuffleArray()).toHaveReturned()
+    // })
     test('shuffleArr should return an Array', () => {
-        expect(shuffleArray().toBeCalled())
+        // expect(shuffleArray().toBeCalled())
+    //class example
+        expect(Array.isArray(shuffleArray(testArr))).toBe(true)
+    })
+    test('checks array of length with returns the same length', () => {
+        expect(shuffleArray(testArr)).toHaveLength(testArr.length)
+    })
+    test('all the same items have the samw items are in the array', () => {
+        //invoce shuffke array func then see if the values equal the same items 
+        expect(shuffleArray(testArr)).toEqual(expect.arrayContaining(testArr))
+    })
+    test('the items have been shuffled', () => {
+        //compairs the result of the function invoked to see if its the same as the same one 
+        let result = shuffleArray(testArr)
+        expect(result.join()).not.toEqual(testArr.join())
     })
 })
